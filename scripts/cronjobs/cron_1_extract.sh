2c30e7275cc40489689b1deef2450a2b82838144
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-09-30 17:05:34 -0500

    cronjobs

--- scripts/cronjobs/cron_1_extract.sh
+++ scripts/cronjobs/cron_1_extract.sh
@@ -1,1 +1,1 @@
-cd /home/henry_grupo10_v1/1_etl && sudo wget -O - https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/2_extract_datoscrudos.bash | bash
+cd ~ && sudo wget -O - https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/2_extract_datoscrudos.bash | bash
