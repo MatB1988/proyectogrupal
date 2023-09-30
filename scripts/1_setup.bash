68ee3a16be9255c1c64d4c064d80a1339d6df1ed
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-09-30 14:40:33 -0500

    Update 1_setup.bash

--- scripts/1_setup.bash
+++ scripts/1_setup.bash
@@ -1,15 +1,15 @@
 # which gcloud
 sudo rm -rf *
-sudo apt update && sudo apt upgrade -y && sudo apt install python3-pip -y && sudo apt-get install tree -y
+sudo apt update -qq -y && sudo apt upgrade -qq -y && sudo apt install python3-pip -qq -y && sudo apt-get install tree -qq -y
 sudo mkdir 0_scripts 1_data && cd 1_data
 sudo mkdir 1_external 2_pipeline 3_output
 cd /home/henry_grupo10_v1/0_scripts
 sudo wget -O python_requirements.txt https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/python_requirements.txt
-sudo apt autoremove
+sudo apt autoremove -y
 cd ~
 
 # creamos entorno de python
-sudo apt install python3.8-venv && sudo apt update && sudo apt upgrade -y
+sudo apt install python3.8-venv
 sudo python3 -m venv /home/henry_grupo10_v1/env_etl
 source /home/henry_grupo10_v1/env_etl/bin/activate
 sudo pip install -r /home/henry_grupo10_v1/0_scripts/python_requirements.txt -y
@@ -17,21 +17,22 @@ deactivate
 
 # instalamos entorno de R
 # update indices
-sudo apt update -qq
+sudo apt update -qq -y
 # install two helper packages we need
-sudo apt install software-properties-common dirmngr
+sudo apt install software-properties-common dirmngr -y
 # add the signing key (by Michael Rutter) for these repos
 # To verify key, run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
 # Fingerprint: E298A3A825C0D65DFD57CBB651716619E084DAB9
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 # add the R 4.0 repo from CRAN -- adjust 'focal' to 'groovy' or 'bionic' as needed
 sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
-sudo apt install r-base -y && sudo apt update #&& sudo apt upgrade -y
-sudo add-apt-repository ppa:c2d4u.team/c2d4u4.0+
+sudo apt install r-base -y
+sudo add-apt-repository ppa:c2d4u.team/c2d4u4.0+ -y
 sudo apt-get install r-cran-tidyverse -y && sudo apt-get install r-cran-tidycensus -y
 cd /home/henry_grupo10_v1/0_scripts
 sudo wget -O r_requirements.R https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/r_requirements.R
 sudo Rscript r_requirements.R
+sudo apt update -qq -y && sudo apt upgrade -qq -y
 cd ~
 sudo tree /home/henry_grupo10_v1/1_data
 
