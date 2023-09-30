938fb2c5673398238282d3063912ebcdc16f0b16
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-09-30 13:09:11 -0500

    setup

--- scripts/1_setup.bash
+++ scripts/1_setup.bash
@@ -1,17 +1,27 @@
 # which gcloud
 sudo rm -rf *
-sudo apt update && sudo apt upgrade -y && sudo apt install python3-pip -y
-sudo apt install python3.8-venv && sudo apt update && sudo apt upgrade -y
-#sudo mkdir 1_etl && cd ~/1_etl
+sudo apt update && sudo apt upgrade -y && sudo apt install python3-pip -y && sudo apt-get install tree -y
 sudo mkdir 0_scripts 1_data && cd 1_data
 sudo mkdir 1_external 2_pipeline 3_output
 cd /home/henry_grupo10_v1/0_scripts
-sudo wget -O requirements.txt https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/requirements.txt
+sudo wget -O python_requirements.txt https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/python_requirements.txt
 cd ~
+
+# creamos entorno de python
+sudo apt install python3.8-venv && sudo apt update && sudo apt upgrade -y
 sudo python3 -m venv /home/henry_grupo10_v1/env_etl
 source /home/henry_grupo10_v1/env_etl/bin/activate
 sudo pip install -r /home/henry_grupo10_v1/0_scripts/requirements.txt
 deactivate
+
+# instalamos entorno de R
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+sudo apt install r-base -y && sudo apt update #&& sudo apt upgrade -y
+cd /home/henry_grupo10_v1/0_scripts
+sudo wget -O r_requirements.R https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/r_requirements.R
+Rscript r_requirements.R
+sudo tree /home/henry_grupo10_v1/1_data
 
 #sudo mkdir 2_ml && cd ~/2_ml
 #sudo mkdir 0_scripts 1_data && cd 1_data
