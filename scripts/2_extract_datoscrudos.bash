adafed23ef1dc70af85d74a3e3d1c36cce1fd919
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-09-30 17:04:29 -0500

    Update 2_extract_datoscrudos.bash

--- scripts/2_extract_datoscrudos.bash
+++ scripts/2_extract_datoscrudos.bash
@@ -4,4 +4,8 @@ sudo rename 'y/A-Z/a-z/' *
 sudo mkdir gmaps && cd gmaps && sudo mkdir reviews_estados metadata_sitios
 cd reviews_estados && sudo gdown --folder --remaining-ok https://drive.google.com/drive/folders/19QNXr_BcqekFNFNYlKd0kcTXJ0Zg7lI6
 cd ..
-cd metadata_sitios && sudo gdown --folder --remaining-ok https://drive.google.com/drive/folders/1olnuKLjT8W2QnCUUwh8uDuTTKVZyxQ0Z
+# gdrive henry
+#cd metadata_sitios && sudo gdown --folder --remaining-ok https://drive.google.com/drive/folders/1olnuKLjT8W2QnCUUwh8uDuTTKVZyxQ0Z
+
+# gdrive manuel
+cd metadata_sitios && sudo gdown --folder --remaining-ok https://drive.google.com/drive/folders/1RN879XLrXdtaXltzijPOmFA2Ir54mWBb
