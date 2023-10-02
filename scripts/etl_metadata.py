91e1e560d6156fd00f9ee4217e09e8ba395c5133
MatB1988 <bassomatias88@gmail.com>
2023-10-02 11:25:41 -0300

    Create etl_metadata.py

---
+++ scripts/etl_metadata.py
@@ -0,0 +1,63 @@
+# Liberias
+import os
+import json
+import numpy as np
+import pandas as pd
+
+# Ruta de la carpeta que contiene los archivos JSON
+ruta_datos = "C:/Users/54280/Documents/GitHub/proyectogrupal/Pruebas/metadata-sitios"
+
+# Lista para almacenar los DataFrames de datos JSON
+data_frames = []
+
+# Itero sobre los archivos JSON en la carpeta
+for filename in os.listdir(ruta_datos):
+    if filename.endswith(".json"):
+        file_path = os.path.join(ruta_datos, filename)
+        # Leo el archivo JSON y creo un DataFrame
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = pd.read_json(file, lines=True)
+        data_frames.append(data)
+
+# Combino todos los DataFrames de datos JSON en uno
+df_gmaps = pd.concat(data_frames, ignore_index=True)
+
+# Saco la lista que compone category y la dejo como texto para explorarla
+df_exploded = df_gmaps.explode('category')
+
+# Lista de palabras clave
+palabras_clave = ["restaurant", "food", "cafe", "diner", "bakery", "lunch", "brunch"]
+
+# Combino palabras clave en un patrón de búsqueda con '|'
+patron_busqueda = '|'.join(palabras_clave)
+
+# Eliminar registros con valores faltantes en la columna "category"
+df_exploded = df_exploded.dropna(subset=['category'])
+
+# Convertir la columna "category" a minúsculas y eliminar espacios en blanco
+df_exploded['category'] = df_exploded['category'].str.lower().str.strip()
+
+# Filtrar los registros que contienen cualquier parte de las palabras clave en la columna "category"
+df_filtrado = df_exploded[df_exploded['category'].str.contains(patron_busqueda, case=False)]
+
+# Genero un df que contenga el los id y los nombres para que se puedan filtrar las rewiews
+df_id_gmaps = df_filtrado[['gmap_id', 'name']]
+
+# Exporto un df con los id y los name para pasar al grupo que esta trabajando con  las rewiews
+df_id_gmaps.to_csv('df_id_gmaps.csv')
+
+# Creo una nueva columna 'estado_categoria' en función de las etiquetas existentes
+def categorizar_estado(row):
+    if pd.notna(row) and ('Open' in row or '24 hours' in row):
+        return 'Open'
+    elif pd.notna(row) and 'Temporarily closed' in row:
+        return 'Temporarily closed'
+    elif pd.notna(row) and 'Permanently closed' in row:
+        return 'Permanently closed'
+    else:
+        return 'Specific Hours'
+
+df_filtrado['estado_categoria'] = df_filtrado['state'].apply(categorizar_estado)
+
+# Exporto df_filtrado para probar union por latitud y longitud con la base de yeld
+df_filtrado.to_csv('gmaps_filtrado.csv')
