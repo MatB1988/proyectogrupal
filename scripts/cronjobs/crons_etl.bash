478ebd5588c2506d8d5ebbaf9cf9c396649417c1
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-10-04 17:30:32 -0500

    Update crons_etl.bash

--- scripts/cronjobs/crons_etl.bash
+++ scripts/cronjobs/crons_etl.bash
@@ -11,8 +11,3 @@ sudo wget https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts
 
 # cron jobs para setup
 wget -O - https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/1_setup_1_etl.bash | bash
-
-# ejemplo crons jobs para ETL
-cd ~/1_etl && wget -O - https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/2_extract_datoscrudos.bash | bash ;
-cd ~/1_data && sudo wget -O transformar_v1.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/3_transformar_v1.py;
-sudo python3 transformar_v1.py && gcloud storage cp ~/3_output/* gs://1_etl_output/ --recursive
