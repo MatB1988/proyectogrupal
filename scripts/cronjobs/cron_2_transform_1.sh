b07095ae866d41ddc9b9cb7f120493744880a68e
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-10-04 11:46:54 -0500

    Update cron_2_transform_1.sh

--- scripts/cronjobs/cron_2_transform_1.sh
+++ scripts/cronjobs/cron_2_transform_1.sh
@@ -1,4 +1,3 @@
-cd ~
-[ -f gmaps_reviews_norm.py ] && sudo rm gmaps_reviews_norm.py
+cd ~ && [ -f gmaps_reviews_norm.py ] && sudo rm gmaps_reviews_norm.py
 sudo wget -O gmaps_reviews_norm.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/4_etl_gmaps_rev_1_trnsfrmr.py
 source /home/henry_grupo10_v1/env_extract/bin/activate && sudo python3 gmaps_reviews_norm.py
