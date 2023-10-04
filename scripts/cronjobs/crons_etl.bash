4b5688b22f07866798f26af2d5f3c50c9a27abb4
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-10-04 17:34:27 -0500

    Update crons_etl.bash

--- scripts/cronjobs/crons_etl.bash
+++ scripts/cronjobs/crons_etl.bash
@@ -1,3 +1,6 @@
+# script para el setup
+wget -O - https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/1_setup_1_etl.bash | bash
+
 # para bajar a servers
 cd ~/0_scripts
 sudo wget https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/cronjobs/cron_1_extract.sh
@@ -8,6 +11,3 @@ sudo wget https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts
 00 03 * * sun sh ~/0_scripts/cron_1_extract.sh >/dev/null 2>&1
 00 04 * * sun sh ~/0_scripts/cron_2_transform.sh >/dev/null 2>&1
 00 05 * * sun sh ~/0_scripts/cron_3_cargar.sh >/dev/null 2>&1
-
-# cron jobs para setup
-wget -O - https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/1_setup_1_etl.bash | bash
