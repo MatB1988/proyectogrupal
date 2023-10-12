4644047a648d8d7cb94813e05b2b4ddc573d8b56
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-10-12 17:25:36 -0500

    Update 9_kpi_gmaps.sh

--- scripts/9_kpi_gmaps.sh
+++ scripts/9_kpi_gmaps.sh
@@ -2,3 +2,8 @@
 cd ~ && [ -f kpi_popularidad_gmaps.py ] && sudo rm kpi_popularidad_gmaps.py
 sudo wget -O kpi_popularidad_gmaps.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/7a_kpi_popularidad_gmaps.py
 source /home/henry_grupo10_v1/env_extract/bin/activate && sudo python3 kpi_popularidad_gmaps.py && deactivate
+
+# kpi_unir_gmaps.py
+cd ~ && [ -f kpi_unir_gmaps.py ] && sudo rm kpi_unir_gmaps.py
+sudo wget -O kpi_unir_gmaps.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/8_kpi_unir_gmaps.py
+source /home/henry_grupo10_v1/env_extract/bin/activate && sudo python3 kpi_unir_gmaps.py && deactivate
