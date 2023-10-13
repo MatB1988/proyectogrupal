fe76a0206dcfd7b63036f7f5942561407b31a34e
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-10-13 11:48:03 -0500

    Update 8a_kpi_unir_gmaps.py

--- scripts/8a_kpi_unir_gmaps.py
+++ scripts/8a_kpi_unir_gmaps.py
@@ -6,11 +6,11 @@ folder_pipeline = "2_pipeline"
 folder_output = "3_output"
 
 # importamos el df
-yelp_kpi_popularidad = pd.read_parquet(
-    os.path.join(folder_pipeline,'yelp_kpi_popularidad.parquet'))
+gmaps_kpi_popularidad = pd.read_parquet(
+    os.path.join(folder_pipeline,'gmaps_kpi_popularidad.parquet'))
 
-yelp_kpi = yelp_kpi_popularidad
-#yelp_kpi = pd.merge(left=yelp_kpi_popularidad,right=yelp_kpi_satisfaccion, how='left')
+gmaps_kpi = gmaps_kpi_popularidad
+#gmaps_kpi = pd.merge(left=gmaps_kpi_popularidad,right=gmaps_kpi_satisfaccion, how='left')
 
-yelp_kpi.to_parquet(
-    os.path.join(folder_output,'yelp_kpi.parquet'))
+gmaps_kpi.to_parquet(
+    os.path.join(folder_output,'gmaps_kpi.parquet'))
