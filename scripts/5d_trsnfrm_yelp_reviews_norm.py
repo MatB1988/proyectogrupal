31dc38ae15a879107ae7b70fa8c3ea0a59e07a86
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-10-13 10:25:30 -0500

    Update 5d_trsnfrm_yelp_reviews_norm.py

--- scripts/5d_trsnfrm_yelp_reviews_norm.py
+++ scripts/5d_trsnfrm_yelp_reviews_norm.py
@@ -14,7 +14,22 @@ with open(os.path.join(
     folder_data,yelp_folder, "review.json"), "r") as file:
         for line in file:
             data.append(json.loads(line))
-yelp_reviews = pd.DataFrame(data)
+data_yelp_reviews = pd.DataFrame(data)
 
-yelp_reviews.to_parquet(
+data_yelp_reviews['user_time_year'] = pd.to_datetime(data_yelp_reviews['date']).dt.year
+data_yelp_reviews['user_time_month'] = pd.to_datetime(data_yelp_reviews['date']).dt.month
+data_yelp_reviews['user_time_day'] = pd.to_datetime(data_yelp_reviews['date']).dt.day
+data_yelp_reviews['user_time_hms'] = pd.to_datetime(data_yelp_reviews['date']).dt.time
+
+data_yelp_reviews_norm = data_yelp_reviews.loc[
+    (data_yelp_reviews['user_time_year'] >= 2020) &
+    (data_yelp_reviews['user_time_month'] >= 7)].copy().sort_values(
+        ["user_time_year","user_time_month"]).drop(
+            columns=["useful","funny","cool"]).rename(columns={
+                "stars":"rating",
+                "text":"user_text",
+                "date":"user_time"
+            })
+
+data_yelp_reviews.to_parquet(
     os.path.join(folder_output,'yelp_reviews.parquet'))
