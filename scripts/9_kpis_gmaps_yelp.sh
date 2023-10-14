8b7c1d61251231b7bb6eff519a13c7c330b17117
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-10-14 15:29:04 -0500

    kpi

--- scripts/9_kpis_gmaps_yelp.sh
+++ scripts/9_kpis_gmaps_yelp.sh
@@ -7,13 +7,3 @@ source /home/henry_grupo10_v1/env_extract/bin/activate && sudo python3 kpi_popul
 cd ~ && [ -f kpi_unir_gmaps.py ] && sudo rm kpi_unir_gmaps.py
 sudo wget -O kpi_unir_gmaps.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/8a_kpi_unir_gmaps.py
 source /home/henry_grupo10_v1/env_extract/bin/activate && sudo python3 kpi_unir_gmaps.py && deactivate
-
-# kpi_popularidad_yelp.py
-cd ~ && [ -f kpi_popularidad_yelp.py ] && sudo rm kpi_popularidad_yelp.py
-sudo wget -O kpi_popularidad_yelp.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/7a_kpi_popularidad_yelp.py
-source /home/henry_grupo10_v1/env_extract/bin/activate && sudo python3 kpi_popularidad_yelp.py && deactivate
-
-# kpi_unir_yelp.py
-cd ~ && [ -f kpi_unir_yelp.py ] && sudo rm kpi_unir_yelp.py
-sudo wget -O kpi_unir_yelp.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/8a_kpi_unir_yelp.py
-source /home/henry_grupo10_v1/env_extract/bin/activate && sudo python3 kpi_unir_yelp.py && deactivate
