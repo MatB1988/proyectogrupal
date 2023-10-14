4717b172e9bd8ad150109e2ff319998fefc4fa1c
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-10-14 16:48:38 -0500

    Update 9_kpis.sh

--- scripts/9_kpis.sh
+++ scripts/9_kpis.sh
@@ -7,3 +7,11 @@ source /home/henry_grupo10_v1/env_extract/bin/activate && sudo python3 kpi_popul
 cd ~ && [ -f kpi_satisfaccion.py ] && sudo rm kpi_satisfaccion.py
 sudo wget -O kpi_satisfaccion.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/7b_kpi_satisfaccion.py
 source /home/henry_grupo10_v1/env_extract/bin/activate && sudo python3 kpi_satisfaccion.py && deactivate
+
+# kpi_cumplimiento.py
+
+
+# kpi_unir.py
+cd ~ && [ -f kpi_unir.py ] && sudo rm kpi_unir.py
+sudo wget -O kpi_unir.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/8_kpi_unir.py
+source /home/henry_grupo10_v1/env_extract/bin/activate && sudo python3 kpi_unir.py && deactivate
