9db12b0a5c399981e35570f25fb2e45f413535e1
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-10-14 11:37:13 -0500

    Update 5f_cnsldr_business_reviews.py

--- scripts/5f_cnsldr_business_reviews.py
+++ scripts/5f_cnsldr_business_reviews.py
@@ -10,11 +10,13 @@ folder_output = "3_output"
 data_gmaps_reviews = pd.read_parquet(
     os.path.join(folder_pipeline,'data_gmaps_reviews_norm.parquet')).rename(
     columns={"gmap_id":"business_id"}).drop(
-        columns=["user_name"])
+        columns=["user_name","user_time",
+                 "resp_text","resp_time",
+                 "resp_time_year","resp_time_month","resp_time_day","resp_time_hms"]).drop_duplicates()
     
 data_yelp_reviews = pd.read_parquet(
     os.path.join(folder_pipeline,'data_yelp_reviews_norm.parquet')).drop(
-        columns=["review_id"])
+    columns=["review_id","user_time"]).drop_duplicates()
 
 data_business_reviews = pd.concat([data_gmaps_reviews, data_yelp_reviews]).rename(
     columns={"zcta5_geoid":"codigo_postal_zcta"})
