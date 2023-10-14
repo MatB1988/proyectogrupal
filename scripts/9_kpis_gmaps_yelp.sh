b3e709f77521239f474685a8ea985922100d8d81
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-10-14 15:30:51 -0500

    Update 9_kpis_gmaps_yelp.sh

--- scripts/9_kpis_gmaps_yelp.sh
+++ scripts/9_kpis_gmaps_yelp.sh
@@ -1,9 +1,9 @@
-# kpi_popularidad_gmaps.py
-cd ~ && [ -f kpi_popularidad_gmaps.py ] && sudo rm kpi_popularidad_gmaps.py
-sudo wget -O kpi_popularidad_gmaps.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/7a_kpi_popularidad_gmaps.py
+# kpi_popularidad.py
+cd ~ && [ -f kpi_popularidad.py ] && sudo rm kpi_popularidad.py
+sudo wget -O kpi_popularidad.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/7a_kpi_popularidad.py
 source /home/henry_grupo10_v1/env_extract/bin/activate && sudo python3 kpi_popularidad_gmaps.py && deactivate
 
-# kpi_unir_gmaps.py
-cd ~ && [ -f kpi_unir_gmaps.py ] && sudo rm kpi_unir_gmaps.py
-sudo wget -O kpi_unir_gmaps.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/8a_kpi_unir_gmaps.py
+# kpi_satisfaccion.py
+cd ~ && [ -f kpi_satisfaccion.py ] && sudo rm kpi_satisfaccion.py
+sudo wget -O kpi_satisfaccion.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/7b_kpi_satisfaccion.py
 source /home/henry_grupo10_v1/env_extract/bin/activate && sudo python3 kpi_unir_gmaps.py && deactivate
