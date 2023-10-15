19b0b27d55a9616406e7234e03c4bfd38cc804da
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-10-15 14:42:30 -0500

    kpi

--- scripts/8_kpi_unir.py
+++ scripts/8_kpi_unir.py
@@ -13,8 +13,14 @@ business_kpi_popularidad = pd.read_parquet(
 business_kpi_satisfaccion = pd.read_parquet(
     os.path.join(folder_pipeline,'business_kpi_satisfaccion.parquet'))
 
+business_kpi_satisfaccion.drop(
+    columns=["kpi_satisfaccion_rating","kpi_satisfaccion_sentimiento"],
+    inplace=True)
+
 business_kpi_sindefinir = business_kpi_popularidad[
-    ["state_name","state_code","codigo_postal_zcta","business_id"]].copy().drop_duplicates()
+    ["state_name","state_code","codigo_postal_zcta","business_id"]].copy()
+business_kpi_sindefinir.drop_duplicates(inplace=True)
+
 n_size = len(business_kpi_sindefinir)
 business_kpi_sindefinir["kpi_3i_sindefinir"] = np.random.normal(loc=60, scale=15.0, size=(n_size))
 
