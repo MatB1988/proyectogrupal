2571e1ee0999405bb3d9eaa9ed09ce68cbfff82d
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-10-16 18:10:49 -0500

    Update 5c_trnsfrm_yelp_metadata_geo.py

--- scripts/5c_trnsfrm_yelp_metadata_geo.py
+++ scripts/5c_trnsfrm_yelp_metadata_geo.py
@@ -4,7 +4,7 @@ import json
 import numpy as np
 import pandas as pd
 import geopandas as geopd
-import pyogrio
+#import pyogrio
 import warnings
 warnings.filterwarnings('ignore', message='.*initial implementation of Parquet.*')
 
@@ -47,7 +47,7 @@ data_yelp_business_filtrado = data_yelp_business.loc[
 geodata_zcta = geopd.read_file(
     os.path.join(
         folder_data,folder_uscensus,folder_zcta_geo,'tl_2020_us_zcta520.shp')
-    ,engine="pyogrio"
+    #,engine="pyogrio"
     ).iloc[:,[1,5,9]]
 geodata_zcta.rename(
     columns = {"GEOID20":"zcta5_geoid",
