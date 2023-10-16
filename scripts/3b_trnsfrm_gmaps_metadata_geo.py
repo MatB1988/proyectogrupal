efc2abc5c48a8ba9244f8b5abeab4201ed562cd5
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-10-16 18:02:07 -0500

    Update 3b_trnsfrm_gmaps_metadata_geo.py

--- scripts/3b_trnsfrm_gmaps_metadata_geo.py
+++ scripts/3b_trnsfrm_gmaps_metadata_geo.py
@@ -4,7 +4,7 @@ import json
 import numpy as np
 import pandas as pd
 import geopandas as geopd
-import pyogrio
+#import pyogrio
 import warnings
 warnings.filterwarnings('ignore', message='.*initial implementation of Parquet.*')
 
@@ -29,7 +29,7 @@ data_gmaps_metadata_sindups = pd.read_parquet(
 geodata_zcta = geopd.read_file(
     os.path.join(
         folder_data,folder_uscensus,folder_zcta_geo,'tl_2020_us_zcta520.shp')
-    ,engine="pyogrio"
+    #,engine="pyogrio"
     ).iloc[:,[1,5,9]]
 geodata_zcta.rename(
     columns = {"GEOID20":"zcta5_geoid",
