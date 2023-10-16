1ef8f0dd2cabb66fb3e88065984e9d5f0383188c
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-10-16 17:50:45 -0500

    Update 2_extract_datoscrudos.bash

--- scripts/2_extract_datoscrudos.bash
+++ scripts/2_extract_datoscrudos.bash
@@ -3,20 +3,19 @@ cd /home/henry_grupo10_v1/1_data_extract
 
 [ -d yelp ] && sudo rm -rf yelp
 # gdrive henry: yelp
-sudo gdown --folder https://drive.google.com/drive/folders/1TI-SsMnZsNP6t930olEEWbBQdo_yuIZF
-sudo rename 'y/A-Z/a-z/' *
+#sudo gdown --folder https://drive.google.com/drive/folders/1TI-SsMnZsNP6t930olEEWbBQdo_yuIZF
+#sudo rename 'y/A-Z/a-z/' *
 # gdrive manuel
-#sudo gdown --folder https://drive.google.com/drive/folders/1ZheujPb0-kmlZEhi_dKo6Ge9o_7dUfEg
+sudo gdown --folder https://drive.google.com/drive/folders/1ZheujPb0-kmlZEhi_dKo6Ge9o_7dUfEg
 
 # gdrive henry: reviews_estados
 sudo mkdir -p gmaps && cd gmaps
-sudo gdown --folder --remaining-ok 19QNXr_BcqekFNFNYlKd0kcTXJ0Zg7lI6
-cd ..
+#sudo gdown --folder --remaining-ok 19QNXr_BcqekFNFNYlKd0kcTXJ0Zg7lI6
 
 # gdrive henry: metadata_sitios
-sudo gdown --folder 1RN879XLrXdtaXltzijPOmFA2Ir54mWBb
+#sudo gdown --folder 1RN879XLrXdtaXltzijPOmFA2Ir54mWBb
 # gdrive manuel: metadata_sitios
-#sudo gdown --folder https://drive.google.com/drive/folders/1olnuKLjT8W2QnCUUwh8uDuTTKVZyxQ0Z
+sudo gdown --folder https://drive.google.com/drive/folders/1olnuKLjT8W2QnCUUwh8uDuTTKVZyxQ0Z
 
 cd /home/henry_grupo10_v1/0_external
 sudo gdown 1_gDjMlGEVETNIG_iixvP5IGKE9Hjy9Vu # census_api.csv
@@ -35,10 +34,11 @@ cd /home/henry_grupo10_v1/downloads
 sudo unzip tl_2020_us_zcta520.zip -d /home/henry_grupo10_v1/1_data_extract/uscensus/zcta_geo/
 
 cd ~
+# copiamos a VM por que limite de transferencia en henry ha sido excedido
+sudo gsutil -m cp -r gs://0_datoscrudos/gmaps/* /home/henry_grupo10_v1/1_data_extract/gmaps/
+
 # actualizamos cloud storage; flags:
 # -m  multi-threaded/multi-processing
 # -r recursive
 # -n not run
-#gsutil -m rsync -r -n /home/henry_grupo10_v1/1_data_extract/ gs://0_datoscrudos/
-#gsutil -m rsync -r /home/henry_grupo10_v1/1_data_extract/ gs://0_datoscrudos/
-sudo gsutil -m cp -r gs://0_datoscrudos/* /home/henry_grupo10_v1/1_data_extract/
+gsutil -m rsync -r /home/henry_grupo10_v1/1_data_extract/ gs://0_datoscrudos/
