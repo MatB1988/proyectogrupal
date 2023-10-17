88e3c5e403ab02831daf0646b2e5ea4e1ba84e87
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-10-17 12:01:10 -0500

    Update 12_kpis.sh

--- scripts/12_kpis.sh
+++ scripts/12_kpis.sh
@@ -1,17 +1,19 @@
 # kpi_popularidad.py
 cd ~ && [ -f kpi_popularidad.py ] && sudo rm kpi_popularidad.py
-sudo wget -O kpi_popularidad.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/7a_kpi_popularidad.py
+sudo wget -O kpi_popularidad.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/10a_kpi_popularidad.py
 sudo ~/anaconda3/envs/pandas_scikit/bin/python kpi_popularidad.py
 
 # kpi_satisfaccion.py
 cd ~ && [ -f kpi_satisfaccion.py ] && sudo rm kpi_satisfaccion.py
-sudo wget -O kpi_satisfaccion.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/7b_kpi_satisfaccion.py
+sudo wget -O kpi_satisfaccion.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/10b_kpi_satisfaccion.py
 sudo ~/anaconda3/envs/pandas_scikit/bin/python kpi_satisfaccion.py
 
 # kpi_cumplimiento.py
-
+cd ~ && [ -f kpi_cumplimiento.py ] && sudo rm kpi_cumplimiento.py
+sudo wget -O kpi_cumplimiento.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/10c_kpi_cumplimiento.py
+sudo ~/anaconda3/envs/pandas_scikit/bin/python kpi_cumplimiento.py
 
 # kpi_unir.py
 cd ~ && [ -f kpi_unir.py ] && sudo rm kpi_unir.py
-sudo wget -O kpi_unir.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/8_kpi_unir.py
+sudo wget -O kpi_unir.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/11_kpi_unir.py
 sudo ~/anaconda3/envs/pandas_scikit/bin/python kpi_unir.py
