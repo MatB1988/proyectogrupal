e719d7e3fe0f08a5df96cbff0476b34baf532f2c
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-10-16 19:16:04 -0500

    Update 11_ml.sh

--- scripts/11_ml.sh
+++ scripts/11_ml.sh
@@ -1,17 +1,4 @@
-# kpi_popularidad.py
-cd ~ && [ -f kpi_popularidad.py ] && sudo rm kpi_popularidad.py
-sudo wget -O kpi_popularidad.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/7a_kpi_popularidad.py
-sudo ~/anaconda3/envs/pandas_scikit/bin/python kpi_popularidad.py
-
-# kpi_satisfaccion.py
-cd ~ && [ -f kpi_satisfaccion.py ] && sudo rm kpi_satisfaccion.py
-sudo wget -O kpi_satisfaccion.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/7b_kpi_satisfaccion.py
-sudo ~/anaconda3/envs/pandas_scikit/bin/python kpi_satisfaccion.py
-
-# kpi_cumplimiento.py
-
-
-# kpi_unir.py
-cd ~ && [ -f kpi_unir.py ] && sudo rm kpi_unir.py
-sudo wget -O kpi_unir.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/8_kpi_unir.py
-sudo ~/anaconda3/envs/pandas_scikit/bin/python kpi_unir.py
+# gmaps_modelo.py
+cd ~ && [ -f gmaps_modelo.py ] && sudo rm gmaps_modelo.py
+sudo wget -O gmaps_modelo.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/10a_gmaps_modelo.py
+sudo ~/anaconda3/envs/pandas_scikit/bin/python gmaps_modelo.py
