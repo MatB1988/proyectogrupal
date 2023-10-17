773aa081b536456c0d9dfad40aec8701bf71d4a6
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-10-17 11:49:55 -0500

    Update 9_ml.sh

--- scripts/9_ml.sh
+++ scripts/9_ml.sh
@@ -1,9 +1,9 @@
 # gmaps_modelo.py
 cd ~ && [ -f gmaps_modelo.py ] && sudo rm gmaps_modelo.py
-sudo wget -O gmaps_modelo.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/10a_gmaps_modelo.py
+sudo wget -O gmaps_modelo.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/7a_gmaps_modelo.py
 sudo ~/anaconda3/envs/pandas_scikit/bin/python gmaps_modelo.py
 
 # ml_unir.py
 cd ~ && [ -f ml_unir.py ] && sudo rm ml_unir.py
-sudo wget -O ml_unir.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/10c_ml_unir.py
+sudo wget -O ml_unir.py https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/8_ml_unir.py
 sudo ~/anaconda3/envs/pandas_scikit/bin/python ml_unir.py
