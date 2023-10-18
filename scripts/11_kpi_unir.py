5bea2975ca128a9e407483245497adf72a5b05ad
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-10-18 10:30:03 -0500

    Update 11_kpi_unir.py

--- scripts/11_kpi_unir.py
+++ scripts/11_kpi_unir.py
@@ -19,27 +19,6 @@ business_kpi_cumplimiento = pd.read_parquet(
 business_kpi_cumplimiento.rename(
     columns={"zcta5_geoid":"codigo_postal_zcta"}, inplace=True)
 business_kpi_cumplimiento.drop(
-    columns=["atributos_business_sum","atributos_cp_max","atributos_state_max"],
-    inplace=True)
-
-# datos ficticios
-# business_kpi_satisfaccion.drop(columns=["kpi_satisfaccion_rating","kpi_satisfaccion_sentimiento"],inplace=True)
-#business_kpi_sindefinir = business_kpi_popularidad[["state_name","state_code","codigo_postal_zcta","business_id"]].copy()
-#business_kpi_sindefinir.drop_duplicates(inplace=True)
-#n_size = len(business_kpi_sindefinir)
-#business_kpi_sindefinir["kpi_3i_sindefinir"] = np.random.normal(loc=60, scale=15.0, size=(n_size))
-
-business_kpi = pd.merge(
-    left=pd.merge(
-        left=business_kpi_popularidad,
-        right=business_kpi_satisfaccion,
-        how='left'),
-    right=business_kpi_cumplimiento,
-    how='left')
-
-business_kpi_cumplimiento.rename(
-    columns={"zcta5_geoid":"codigo_postal_zcta"}, inplace=True)
-business_kpi_cumplimiento.drop(
     columns=["atributos_business_sum","atributos_cp_max","atributos_state_max",
              "codigo_postal_zcta","state_code"],
     inplace=True)
