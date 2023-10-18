85adee40d4c561264c83e9ec4431858956f66f99
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2023-10-17 20:47:46 -0500

    Update cron_2_transform.sh

--- scripts/cronjobs/cron_2_transform.sh
+++ scripts/cronjobs/cron_2_transform.sh
@@ -1,3 +1,3 @@
 cd ~ && sudo wget -O - https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/6_trnsfrm_gmaps_yelp.sh | bash ;
-cd ~ && sudo wget -O - https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/9_mil.sh | bash
+cd ~ && sudo wget -O - https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/9_ml.sh | bash
 cd ~ && sudo wget -O - https://raw.githubusercontent.com/MatB1988/proyectogrupal/main/scripts/12_kpis.sh | bash
